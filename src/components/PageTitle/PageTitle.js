75960df103fa3683564cd3f014d51021f7115529
Olifan <olifananton@gmail.com>
2022-09-21 01:18:53 +0300

    Make PageTitle flex

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './PageTitle.module.css';
+import PalaroidPhoto from '../PalaroidPhoto/PalaroidPhoto';
 
 const PageTitle = () => (
   <div className={styles.PageTitle}>
-    <img src='logo192.png'/>
-    <h1>mycamp</h1>
+    <div>
+      <PalaroidPhoto/>
+    </div>
+    
+    <div>
+      <img src='logo192.png'/>
+    </div>
+    <div>
+      <h1>mycamp</h1>
+      <h6>Дитячий табір в Карпатах та Європі</h6>
+    </div>
+    
+    <div>
+      <PalaroidPhoto/>
+    </div>
+    
   </div>
 );
 
