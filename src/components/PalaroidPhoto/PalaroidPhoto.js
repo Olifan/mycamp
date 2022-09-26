38da43504939230cbb03e0dcc743f97c246961c7
Olifan <olifananton@gmail.com>
2022-09-26 22:15:26 +0300

    Add Props to PolaroidPhoto

--- src/components/PalaroidPhoto/PalaroidPhoto.js
+++ src/components/PalaroidPhoto/PalaroidPhoto.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './PalaroidPhoto.module.css';
 
-const PalaroidPhoto = () => (
+const PalaroidPhoto = ({caption}) => (
   <div className={styles.PalaroidPhoto}>
     <div className={styles.wrapper}>
       <div className={styles.item}>
         <div className={styles.polaroid}>
           <img src='testPhoto.jpeg'/>
           <div className={styles.caption}>
-            Summer 2022
+            {caption}
           </div>
         </div>  
       </div> 
@@ -19,6 +19,8 @@ const PalaroidPhoto = () => (
 
 PalaroidPhoto.propTypes = {};
 
-PalaroidPhoto.defaultProps = {};
+PalaroidPhoto.defaultProps = {
+  caption: "Summer 2022"
+};
 
 export default PalaroidPhoto;
