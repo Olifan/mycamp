c621bb279685175c579a3bccf04ffd893466d3f0
Olifan <olifananton@gmail.com>
2022-10-01 00:26:15 +0300

    Staring CardNavigation

--- src/components/CardNavigation/CardNavigation.js
+++ src/components/CardNavigation/CardNavigation.js
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CardNavigation.module.css';
 
-const CardNavigation = () => (
-  <div className={styles.CardNavigation}>
-    CardNavigation Component
-  </div>
+const CardNavigation = ({linkTitle, linkDescription, linkIcon}) => (
+  <a className={styles.CardNavigation} href="#">
+    <img className={styles.linkIcon} src={linkIcon}/> 
+    <div className={styles.linkContent}>
+      <h2>{linkTitle}</h2>
+      <p className={styles.linkDescription}>{linkDescription}</p>
+    </div>
+  </a>
 );
 
 CardNavigation.propTypes = {};
