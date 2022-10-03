8fb197ffdcb335c4667aa40a74ad9c867084c83e
Olifan <olifananton@gmail.com>
2022-10-03 16:24:02 +0300

    Create RegistrationForm component

---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './RegistrationForm.module.css';
+
+const RegistrationForm = () => (
+  <div className={styles.RegistrationForm}>
+    RegistrationForm Component
+  </div>
+);
+
+RegistrationForm.propTypes = {};
+
+RegistrationForm.defaultProps = {};
+
+export default RegistrationForm;
