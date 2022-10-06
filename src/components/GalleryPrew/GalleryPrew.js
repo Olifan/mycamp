bfb0cf84db4e776289ce98b3880aabf88ed1b145
Olifan <olifananton@gmail.com>
2022-10-06 22:23:52 +0300

    Add mini preww

--- src/components/GalleryPrew/GalleryPrew.js
+++ src/components/GalleryPrew/GalleryPrew.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './GalleryPrew.module.css';
 
-const GalleryPrew = ({title, linkToGallery, coverImg}) => (
+const GalleryPrew = ({title, linkToGallery, coverImg, miniPrewItem}) => (
   <div className={styles.galleryPrew}>
     <h4 className={styles.title}>{title}</h4> 
     <a href={linkToGallery}>
       <img className={styles.galleryCover} src={coverImg}/>
     </a>
+    <div className={styles.miniPrews}>
+      <img className={styles.miniPrewItem} src={miniPrewItem}/>
+    </div>
   </div>
 );
 
