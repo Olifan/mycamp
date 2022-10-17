2bb9a32bea401c95357a9ca9afbb92d8a4c8851f
Olifan <olifananton@gmail.com>
2022-10-18 00:18:14 +0300

    Create OurTeam page and display data on it

---
+++ src/pages/OurTeam/OurTeam.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+import styles from './OurTeam.module.css';
+import ContentService from '../../services/ContentService';
+
+const OurTeam = () => {
+  const contentService = new ContentService();
+
+  const [data, setData] = useState(null);
+
+  useEffect( () => {
+    contentService.getOurTeam().then( (response) => {
+      setData(response);
+    });
+  }, [] );
+
+  return(
+    <div>
+      {data && (
+        <TeamMember
+          memberPhoto={data.data.attributes.teamMember.photo.data.attributes.url}
+          memberName={data.data.attributes.teamMember.name}
+          memberTitle={data.data.attributes.teamMember.title}
+          stickerTapeSrc="https://assets.website-files.com/62e852b3b3432f63a22b8844/62fcbcc5bc7fe93e7ca976b6_st-3.png"
+        />
+      )}
+    </div>
+  );
+};
+
+OurTeam.propTypes = {};
+
+OurTeam.defaultProps = {};
+
+export default OurTeam;
