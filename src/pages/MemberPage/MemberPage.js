c1256b61651cc89cfc2254da9459ad90361d7f83
Olifan <olifananton@gmail.com>
2022-10-25 23:10:40 +0300

    Create MemberPage component

---
+++ src/pages/MemberPage/MemberPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MemberPage.module.css';
+
+const MemberPage = () => (
+  <div className={styles.MemberPage}>
+    MemberPage Component
+  </div>
+);
+
+MemberPage.propTypes = {};
+
+MemberPage.defaultProps = {};
+
+export default MemberPage;
