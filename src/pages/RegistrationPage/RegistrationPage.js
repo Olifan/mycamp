83cbb91460a7d01ee4f2dedad57aa8c6614dd063
Olifan <olifananton@gmail.com>
2022-11-05 00:31:30 +0200

    Create RegistrationPage

---
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './RegistrationPage.module.css';
+
+const RegistrationPage = () => (
+  <div className={styles.RegistrationPage}>
+    RegistrationPage Component
+  </div>
+);
+
+RegistrationPage.propTypes = {};
+
+RegistrationPage.defaultProps = {};
+
+export default RegistrationPage;
