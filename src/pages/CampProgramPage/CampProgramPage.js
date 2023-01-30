86f31f1448b949cd4afc520776918204160cdb18
Olifan <olifananton@gmail.com>
2023-01-30 19:58:22 +0200

    Create CampProgramPage

---
+++ src/pages/CampProgramPage/CampProgramPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CampProgramPage.module.css';
+
+const CampProgramPage = () => (
+  <div className={styles.CampProgramPage}>
+    CampProgramPage Component
+  </div>
+);
+
+CampProgramPage.propTypes = {};
+
+CampProgramPage.defaultProps = {};
+
+export default CampProgramPage;
