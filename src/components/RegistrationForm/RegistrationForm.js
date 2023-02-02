5f5297b53cc2def60539f6efb0b00645dd9bb4f3
Olifan <olifananton@gmail.com>
2023-02-02 11:08:24 +0200

    Add email validation

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -3,6 +3,7 @@ import PhoneInput from 'react-phone-input-2';
 // import 'react-phone-input-2/lib/bootstrap.css'
 import styles from './RegistrationForm.module.css';
 import ContentService from '../../services/ContentService';
+import { useForm } from 'react-hook-form';
 
 
 const RegistrationForm = () => {
@@ -13,8 +14,9 @@ const RegistrationForm = () => {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [shift, setShift] = useState('');
-
   const [shifts, setShifts] = useState('');
+
+  const {register, handleSubmit, formState: { errors }} = useForm();
 
   useEffect( () => {
     contentService.getShift().then((response) => {
@@ -24,15 +26,15 @@ const RegistrationForm = () => {
 
 
 
-  const handleSubmit = async(e) => {
-    e.preventDefault();
+  const onSubmit = async(e) => {
+    // e.preventDefault();
     
     contentService.postRequest({name, email, phone, shift})  
 
     setName('');
     setEmail('');
     setPhone('');
-    // setShift('');
+    setShift('');
   };
 
   return(
@@ -40,14 +42,15 @@ const RegistrationForm = () => {
      <img className={styles.icon} loading="lazy" alt='' src="https://assets.website-files.com/62e852b3b3432f63a22b8844/62eb8e9bdc4b4d07b438002f_streamlinehq-interface-file-clipboard-text-interface-essential-250-2.SVG"/>
      <h2>Registration Form</h2>
      <div className={styles.form}>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label for="name">Name</label>
             <input id="name" value={name} onChange={(e) => setName(e.target.value)} className={styles.input} type="text" name='name' maxLength="256" data-name="Name" placeholder=''/>
           </div>
           <div>
             <label for="email">Email</label>
-            <input id="email" value={email} onChange={(e) => setEmail(e.target.value)} className={styles.input} type="email" name="email" maxLength="256" data-name="Email" placeholder=''/>
+            <input id="email" {...register("email", {required: true, pattern: {value: /^\S+@\S+\.\S+$/, message: "Invalid"}})} value={email} onChange={(e) => setEmail(e.target.value)} className={styles.input} type="text" name="email" maxLength="256" data-name="Email" placeholder=''/>
+            {errors.email && <p>Invalid</p>}
           </div>
           <div>
             <label for="phone">Phone number</label>
