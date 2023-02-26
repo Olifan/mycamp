ec10574ee211b0226d3cf15b09cfe23ca19444b1
Olifan <olifananton@gmail.com>
2023-02-26 14:27:05 +0200

    Create ThingsPage

---
+++ src/pages/ThingsPage/ThingsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ThingsPage.module.css';
+
+const ThingsPage = () => (
+  <div className={styles.ThingsPage}>
+    ThingsPage Component
+  </div>
+);
+
+ThingsPage.propTypes = {};
+
+ThingsPage.defaultProps = {};
+
+export default ThingsPage;
