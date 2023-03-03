ed50c3e7c5087150d66f2a4e0252bc89cd4d0adb
Olifan <olifananton@gmail.com>
2023-03-04 00:28:12 +0200

    Add structure to ContactsPage

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ContactsPage.module.css';
+import PageTitle from '../../components/PageTitle/PageTitle';
+import Registration from '../../components/Registration/Registration';
+import Footer from '../../components/Footer/Footer';
 
-const ContactsPage = () => (
-  <div className={styles.ContactsPage}>
-    ContactsPage Component
-  </div>
-);
+const ContactsPage = () => {
+  return(
+    <div className={styles.ContactsPage}>
+      <div>
+        <PageTitle
+          title="Контакти"
+          description="Today. Tomorrow. Allways."
+        />
+      </div>
+      <div>
+      <CardWithText
+          title="Контакти"
+          text="Текст риба"
+          icon="https://assets.website-files.com/62e852b3b3432f63a22b8844/62ea61cb131e422cf017262d_streamlinehq-shopping-business-target-money-shopping-250.SVG"
+        />
+        <CardWithText
+          title="Контакти"
+          text="Текст риба"
+          icon="https://assets.website-files.com/62e852b3b3432f63a22b8844/62ea61cb131e422cf017262d_streamlinehq-shopping-business-target-money-shopping-250.SVG"
+        />
+      </div>
+      <Registration
+        registrationDate="21-29 грудня"
+        registrationOpen="Реєстрація відкрита"
+        registrationSeason="Зима 2023"
+      />
+      <Footer/>
+    </div>
+  )
+ 
+};
 
 ContactsPage.propTypes = {};
 
