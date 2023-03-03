4bc12933cc9e3460b31552970972f11e31cd9a9b
Olifan <olifananton@gmail.com>
2023-03-03 23:59:52 +0200

    Generate ContactsPage component

---
+++ src/pages/ContactsPage/ContactsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContactsPage.module.css';
+
+const ContactsPage = () => (
+  <div className={styles.ContactsPage}>
+    ContactsPage Component
+  </div>
+);
+
+ContactsPage.propTypes = {};
+
+ContactsPage.defaultProps = {};
+
+export default ContactsPage;
