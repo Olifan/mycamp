6f47fe959264154a3734eafe0f89603b1225d8a3
Olifan <olifananton@gmail.com>
2023-03-10 18:23:19 +0200

    fix css for PhotoPage

--- src/pages/PhotoPage/PhotoPage.js
+++ src/pages/PhotoPage/PhotoPage.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './PhotoPage.module.css';
+import PageTitle from '../../components/PageTitle/PageTitle';
+import GalleryPrew from '../../components/GalleryPrew/GalleryPrew';
 
-const PhotoPage = () => (
-  <div className={styles.PhotoPage}>
-    PhotoPage Component
-  </div>
-);
+const PhotoPage = () => {
+  return(
+    <div className={styles.photoPage}>
+      <PageTitle
+        title="Фото"
+        description="Today. Tomorrow. Allways."
+      />
+      <div className={styles.photoPageContent}>
+        <GalleryPrew
+          title="Summer"
+          coverImg="testPhoto.jpeg"
+          miniPrewItems={["testPhoto.jpeg", "testPhoto.jpeg", "testPhoto.jpeg"]}
+        />
+        <GalleryPrew
+          title="Winter"
+          coverImg="testPhoto.jpeg"
+          miniPrewItems={["testPhoto.jpeg", "testPhoto.jpeg", "testPhoto.jpeg"]}
+        />
+      </div>
+
+    </div>
+  );
+};
 
 PhotoPage.propTypes = {};
 
