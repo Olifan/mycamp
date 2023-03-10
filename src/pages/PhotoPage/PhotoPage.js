58f960e4ca31db9902470a9da1168c2cb15016ba
Olifan <olifananton@gmail.com>
2023-03-10 17:13:52 +0200

    Create PhotoPage

---
+++ src/pages/PhotoPage/PhotoPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './PhotoPage.module.css';
+
+const PhotoPage = () => (
+  <div className={styles.PhotoPage}>
+    PhotoPage Component
+  </div>
+);
+
+PhotoPage.propTypes = {};
+
+PhotoPage.defaultProps = {};
+
+export default PhotoPage;
