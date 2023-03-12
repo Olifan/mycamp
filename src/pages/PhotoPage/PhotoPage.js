a53075caf27171be8546cfa61ef16872d375c377
Olifan <olifananton@gmail.com>
2023-03-12 13:16:35 +0200

    Add lightbox to PhotoPage

--- src/pages/PhotoPage/PhotoPage.js
+++ src/pages/PhotoPage/PhotoPage.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './PhotoPage.module.css';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import GalleryPrew from '../../components/GalleryPrew/GalleryPrew';
+import FsLightbox from "fslightbox-react";
+
 
 const PhotoPage = () => {
+
+  const toogle = false;
+
+  const [showSlider, setShowSlider] = useState(toogle);
+
+  const handleClick = () => {
+    setShowSlider(!showSlider);
+  }
+
   return(
     <div className={styles.photoPage}>
       <PageTitle
         title="Фото"
         description="Today. Tomorrow. Allways."
       />
-      <div className={styles.photoPageContent}>
-        <GalleryPrew
-          title="Summer"
-          coverImg="testPhoto.jpeg"
-          miniPrewItems={["testPhoto.jpeg", "testPhoto.jpeg", "testPhoto.jpeg"]}
-        />
+      
+      <div  className={styles.photoPageContent}>
+        <div onClick = {handleClick}>
+          <GalleryPrew
+            title="Summer"
+            coverImg="testPhoto.jpeg"
+            miniPrewItems={["testPhoto.jpeg", "testPhoto.jpeg", "testPhoto.jpeg"]}
+          />
+        </div>
+        
         <GalleryPrew
           title="Winter"
           coverImg="testPhoto.jpeg"
           miniPrewItems={["testPhoto.jpeg", "testPhoto.jpeg", "testPhoto.jpeg"]}
+        />
+        <FsLightbox
+          toggler = {showSlider}
+          sources = {['testPhoto.jpeg', 'testPhoto.jpeg']}
         />
       </div>
 
