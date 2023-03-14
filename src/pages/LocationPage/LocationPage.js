3c0c43fdbc167e05792c99ee91325e8288f59a1f
Olifan <olifananton@gmail.com>
2023-03-14 10:33:03 +0200

    Create LocationPage

---
+++ src/pages/LocationPage/LocationPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './LocationPage.module.css';
+
+const LocationPage = () => (
+  <div className={styles.LocationPage}>
+    LocationPage Component
+  </div>
+);
+
+LocationPage.propTypes = {};
+
+LocationPage.defaultProps = {};
+
+export default LocationPage;
