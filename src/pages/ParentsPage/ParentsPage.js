3fd9081056791f245b64fd2fe7ed81b12c0a0191
Olifan <olifananton@gmail.com>
2023-03-20 22:56:29 +0200

    Add structure for ParentsPage

--- src/pages/ParentsPage/ParentsPage.js
+++ src/pages/ParentsPage/ParentsPage.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './ParentsPage.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ParentsPage.module.css";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import CardWithText from "../../components/CardWithText/CardWithText";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import Registration from "../../components/Registration/Registration";
+import Footer from "../../components/Footer/Footer";
 
 const ParentsPage = () => {
-  return(
+	return (
     <div className={styles.parentsPage}>
-    ParentsPage Component
-  </div>
-  )
-  
+
+      <PageTitle
+        title="Для батьків"
+        description="Today. Tomorrow. Allways."
+      />
+
+      <div className={styles.parentsContent}>
+        <CardWithText
+          title={""}
+          text={<ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            children={""}
+          />}
+          icon={""}
+        />
+        <CardWithText
+          title={""}
+          text={<ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            children={""}
+          />}
+          icon={""}
+        />
+      </div>
+
+      <Registration
+				registrationDate="21-29 грудня"
+				registrationOpen="Реєстрація відкрита"
+				registrationSeason="Зима 2023"
+			/>
+
+			<Footer />
+    </div>
+  );
 };
 
 ParentsPage.propTypes = {};
