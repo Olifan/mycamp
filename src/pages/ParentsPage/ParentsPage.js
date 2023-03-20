de69275625991e4fd69362e2037d1eb6b5af97b7
Olifan <olifananton@gmail.com>
2023-03-20 17:46:12 +0200

    Create ParentsPage component

---
+++ src/pages/ParentsPage/ParentsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ParentsPage.module.css';
+
+const ParentsPage = () => (
+  <div className={styles.ParentsPage}>
+    ParentsPage Component
+  </div>
+);
+
+ParentsPage.propTypes = {};
+
+ParentsPage.defaultProps = {};
+
+export default ParentsPage;
